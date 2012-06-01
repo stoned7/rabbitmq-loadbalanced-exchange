d66504067f3d869c6836bfad900c80ce89d893a1
sujan <sujandutta@gmail.com>
2012-06-01 14:39:20 +0530

    adding test

--- src/rabbit_exchange_type_loadbalanced.erl
+++ src/rabbit_exchange_type_loadbalanced.erl
@@ -1,3 +1,19 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License
+%% at http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and
+%% limitations under the License.
+%%
+%% The Original Code is Rabbitmq Loadbalanced Exchange.
+%%
+%% The Initial Developer of the Original Code is Tavisca Solution Pvt Ltd.
+%% Copyright (c) 2011-2012 Tavisca Solution Pvt Ltd.  All rights reserved.
+%%
+
 -module(rabbit_exchange_type_loadbalanced).
 -author("sujandutta@gmail.com").
 
--- test/rabbit_exchange_type_loadbalanced_test.erl
+++ test/rabbit_exchange_type_loadbalanced_test.erl
@@ -1,4 +1,21 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License
+%% at http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and
+%% limitations under the License.
+%%
+%% The Original Code is Rabbitmq Loadbalanced Exchange.
+%%
+%% The Initial Developer of the Original Code is Tavisca Solution Pvt Ltd.
+%% Copyright (c) 2011-2012 Tavisca Solution Pvt Ltd.  All rights reserved.
+%%
+
 -module(rabbit_exchange_type_loadbalanced_test).
+-author("sujandutta@gmail.com").
 
 -export([test/0]).
 -include_lib("amqp_client/include/amqp_client.hrl").
